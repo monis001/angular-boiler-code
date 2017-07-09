ca94a4c9d84617235ba5bbdc9d630756e30caa50
monis01 <mnskhn7997@gmail.com>
2017-07-09 23:28:10 +0530

    app.js and index.html updates

--- src/app.js
+++ src/app.js
@@ -1,4 +1,24 @@
+//@ IMP :  https://stackoverflow.com/questions/38374145/what-is-the-calling-order-of-angularjs-functions-config-run-controller
+//@ will be helpful
+//@ https://docs.angularjs.org/guide/module
 angular.module('myApp',[
-	'restangular',
-	'ui.router'
+	'ui.router',
+	'myAppName.restclient',
+	'myApp.api',
+	'myAppName.restclient',
+	'login',
+	'home'
 	])
+	.config(function($stateProvider,$locationProvider){
+		//@ app config gets setup
+		//@ third party modules get's setup here 
+
+
+		// $locationProvider.html5Mode(true);
+		$locationProvider.hashPrefix('');
+		
+	})
+	.run(function($rootScope, $state){
+		$rootScope.$state = $state;
+	})
+	.constant('HOME_DIRECTORY_URL','src/app/')
